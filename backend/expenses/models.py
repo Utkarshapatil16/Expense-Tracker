440fc65c71db6f86c64379e66db8b123c63321f4
Utkarshapatil16 <utkarshapatil510@gmail.com>
2026-05-03 14:53:31 +0530

    added frontend properly

---
+++ backend/expenses/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here
+class Expense(models.Model):
+    title = models.CharField(max_length=100)
+    amount = models.FloatField()
+    category = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ backend/expenses/views.py
@@ -0,0 +1,24 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Expense
+from .serializers import ExpenseSerializer
+
+@api_view(['GET'])
+def get_expenses(request):
+    expenses = Expense.objects.all()
+    serializer = ExpenseSerializer(expenses, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def add_expense(request):
+    serializer = ExpenseSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['DELETE'])
+def delete_expense(request, id):
+    expense = Expense.objects.get(id=id)
+    expense.delete()
+    return Response({"message": "Deleted"})
